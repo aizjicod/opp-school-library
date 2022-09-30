ee676ab2812c4ac7d9c4fd9b679e65266620907f
Aiziji <aletv1501@gmail.com>
2022-09-30 13:45:50 -0400

    Fix linters

--- main.rb
+++ main.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
 require_relative 'app'
-def main 
+def main
   app = App.new
-  puts "Welcome to School Library App"
+  puts 'Welcome to School Library App'
   loop do
     puts "\nPlease select an option by number :"
     puts [
@@ -17,25 +17,25 @@ def main
       '     [7] Exit',
       ''
     ]
-      value = gets.chomp
-      case value
-      when '1'
-        app.books
-      when '2'
-        app.persons
-      when '3'
-        app.create_person
-      when '4'
-        app.create_book
-      when '5'
-        app.create_rental
-      when '6'
-        app.rentals_by_per_id
-      else
-        puts "exiting..."
-        break
-      end
+    value = gets.chomp
+    case value
+    when '1'
+      app.books
+    when '2'
+      app.persons
+    when '3'
+      app.create_person
+    when '4'
+      app.create_book
+    when '5'
+      app.create_rental
+    when '6'
+      app.rentals_by_per_id
+    else
+      puts 'exiting...'
+      break
     end
   end
+end
 
 main
